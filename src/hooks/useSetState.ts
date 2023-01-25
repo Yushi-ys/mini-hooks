48295659c2e71db8b88eb610772ba4fd0dd7251c
Yushi-ys <2540258512@qq.com>
2023-01-25 23:22:32 +0800

    fix: 对useSetState的修改

--- src/hooks/useSetState.ts
+++ src/hooks/useSetState.ts
@@ -3,23 +3,22 @@ import { isFunction } from "../utils";
 
 type obj = Record<string, any>;
 type SetState<S extends obj> = <K extends keyof S>(
-  state: Pick<S, K> | ((prevState: Readonly<S>) => Pick<S, K>)
+  state: Pick<S, K> | null | ((prev: S) => Pick<S, K> | S | null)
 ) => void;
 
-const useSetState = <S extends obj>(
-  initState: S | (() => S)
-): [S, SetState<S>] => {
-  const [state, setState] = useState<S>(initState);
+const useSetState = <T extends obj>(
+  initState: T | (() => T)
+): [T, SetState<T>] => {
+  const [state, setState] = useState<T>(initState);
 
-  const setMergeState = useCallback((param: obj | null) => {
+  const mergeState = useCallback((param: obj | null) => {
     setState((preState) => {
-      console.log(preState);
-      const newState = isFunction(param) ? param(preState) : param;
-      return newState ? { ...preState, ...newState } : preState;
+      const newState = isFunction(param) ? param(preState) : preState;
+      return newState;
     });
   }, []);
 
-  return [state, setMergeState];
+  return [state, mergeState];
 };
 
 export default useSetState;
